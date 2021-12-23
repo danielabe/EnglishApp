88f2a4b9454b081f92d98ccd26613edfd7cbde9a
danielabe <danielaberardi@live.com.ar>
2021-12-22 23:52:23 -0300

    add header and sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 const input = document.getElementById('search')
-const sendBtn = document.querySelector('input[type="submit"]')
-const list = document.querySelector('ul')
+const sendBtn = document.querySelector('.search-btn')
+const list = document.getElementById('words')
 
 const urlBase = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
-sendBtn.addEventListener('click', () => getWord(urlBase, input.value))
+sendBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    getWord(urlBase, input.value)
+})
 
 function getWord(url, word){
     list.innerHTML = ''
@@ -19,7 +22,7 @@ function render(info){
     console.log(info)
     info.forEach((element,i) => {
         list.innerHTML +=   `<li>
-                                ${element.word} ${element.phonetic} 
+                                <h3>${element.word} <span>/${element.phonetic}/</span></h3> 
                                 <audio controls>
                                     <source src=${element.phonetics[0].audio} type="audio/mp3">
                                         Tu navegador no soporta audio HTML5.
@@ -29,13 +32,13 @@ function render(info){
 
         element.meanings.forEach((meaning,j) => {
             document.querySelector(`.meaning-${i}`).innerHTML +=    `<li>
-                                                                        ${meaning.partOfSpeech}
-                                                                        <ul class="definition-${i}${j}"></ul>
+                                                                        <p class="part-of-speech">${meaning.partOfSpeech}</p>
+                                                                        <ul class="definition-list definition-${i}${j}"></ul>
                                                                     </li>`
 
             meaning.definitions.forEach(def => document.querySelector(`.definition-${i}${j}`).innerHTML +=   `<li>
-                                                                                                                    <p>${def.definition}</p>
-                                                                                                                    <p>Example: ${def.example}</p>
+                                                                                                                    <p class="definition">${def.definition}</p>
+                                                                                                                    <p class="example">${def.example}</p>
                                                                                                                     <p>Synonyms: ${def.synonyms.join(', ')}</p>
                                                                                                                     <p>Antonyms: ${def.antonyms}</p>
                                                                                                                 </li>`)
